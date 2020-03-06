ef7ccbef00b74ce3e9bb212167952aeda166a43a
recafox <recafox@gmail.com>
2020-03-07 00:31:24 +0800

    first deploy

---
+++ README.md
@@ -0,0 +1,13 @@
+# JS - Timer
+
+這是一個簡易的計時器，使用到JS，以及SCSS
+
+### 這個作品練習了哪些東西？
+
+- Class，嘗試實作Class-based design
+- SCSS
+- BEM & OOCSS設計模式
+
+#### inspired by 
+
+(The Modern JavaScript Bootcamp Course)[https://www.udemy.com/course/javascript-beginners-complete-tutorial/]
---
+++ timer.js
@@ -0,0 +1,103 @@
+class Timer{
+    constructor(inputMin,inputSec, startBtn, pauseBtn, resetBtn, callbacks){
+        this.inputMin =inputMin;
+        this.inputSec =inputSec;  
+        this.totalDuration;
+        this.timeLeft;
+        this.resetNum = 0;
+        this.startBtn = startBtn;
+        this.pauseBtn = pauseBtn;
+        this.resetBtn = resetBtn;
+        this.disabledStatus = false;
+        if(callbacks){
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+            this.onReset = callbacks.onReset;
+        }
+        this.startBtn.addEventListener('click', this.start);
+        this.pauseBtn.addEventListener('click', this.pause);
+        this.resetBtn.addEventListener('click', this.reset);
+        this.inputMin.addEventListener('change', this.setTotal);
+        this.inputSec.addEventListener('change', this.setTotal);
+        this.reset();
+    }
+    
+    setTotal = () =>{
+        let second;
+        let minute;
+        if(this.inputMin.value.length===0){
+            this.inputMin.value = 0;
+        }
+        if(this.inputSec.value.length===0){
+            this.inputSec.value = 0;
+        }
+        if(parseInt(this.inputMin.value) < 0){
+            this.inputMin.value = 0;
+        }
+        if(parseInt(this.inputSec.value) < 0){
+            this.inputSec.value = 0;
+        }
+
+        if(parseInt(this.inputSec.value)>=60){
+            this.inputSec.value = 60;
+            second = parseInt(this.inputSec.value);
+        }else {
+            second = parseInt(this.inputSec.value);
+        }
+
+        minute = parseInt(this.inputMin.value);
+        this.totalDuration = minute*60 + second;
+        this.timeLeft = this.totalDuration;
+    };
+    start = ()=>{
+        this.disabledStatus = true;
+        if(this.disabledStatus){
+            this.startBtn.setAttribute('disabled', 'disabled');
+            this.inputMin.setAttribute('disabled', 'disabled');
+            this.inputSec.setAttribute('disabled', 'disabled');
+            this.resetBtn.setAttribute('disabled','disabled');
+        }
+        if(this.onStart){
+            this.onStart(this.totalDuration);
+        }  
+        this.intervalID = setInterval(this.tick, 50);  
+    };
+    tick = ()=>{
+        
+        if(this.timeLeft <=0){
+            this.pause();
+            this.reset();
+        }else{ 
+            this.timeLeft = this.timeLeft -0.05;
+            if(this.timeLeft <0){
+                this.timeLeft = 0;
+            }
+            this.inputMin.value = parseInt(this.timeLeft/60);
+            this.inputSec.value = parseInt(this.timeLeft%60);
+            if(this.onTick){
+                this.onTick(this.timeLeft);
+            }
+        }
+       
+    }
+    pause = ()=>{
+        clearInterval(this.intervalID);
+        this.disabledStatus = false;
+        if(this.disabledStatus === false){
+            this.startBtn.removeAttribute('disabled');
+            this.inputMin.removeAttribute('disabled');
+            this.inputSec.removeAttribute('disabled');
+            this.resetBtn.removeAttribute('disabled');
+        };
+        
+    };
+    reset = ()=>{
+        this.inputMin.value = this.resetNum;
+        this.inputSec.value = this.resetNum;
+        if(this.onReset){
+            this.onReset();
+        }
+    };
+
+ };
